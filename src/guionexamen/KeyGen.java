dc5de7dae21ebc9708b5a5ca1545832f0445393c
Miguel-hash85 <msanchezherrero7@gmail.com>
2022-01-17 10:35:26 +0100

    Guion para el examen

---
+++ src/guionexamen/KeyGen.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guionexamen;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 2dam
+ */
+public class KeyGen {
+
+    /**
+     * @param args the command line arguments
+     */
+    //private static final byte[] salt = "esta es la salt!".getBytes();
+    public static void main(String[] args) {
+        
+        try {
+            
+            //System.out.println("Generando clave Blowfish...");
+            KeyPairGenerator generador = KeyPairGenerator.getInstance("RSA");
+            generador.initialize(1024);
+            KeyPair clave = generador.genKeyPair();
+            //System.out.println("Formato: "+claveBlowfish.getFormat());
+            //String publicKey = "mvLBiZsiTbGwrfJB";
+            //String privateKey = "llllllllllllllll";
+            fileWriter("keypublic.txt", clave.getPublic().getEncoded());
+            fileWriter("keyprivate.txt",clave.getPrivate().getEncoded());
+            String data = "lB2eJ0";
+            String cypherPass=KeyManager.encrypt(data);
+            System.out.println(cypherPass);
+            System.out.println(KeyManager.decrypt(cypherPass));
+            
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(KeyGen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+          catch (Exception ex) {
+            Logger.getLogger(KeyGen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	
+        
+    }
+    public static void fileWriter(String path, byte[] text) {
+        try (FileOutputStream fos = new FileOutputStream(path)) {
+                fos.write(text);
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }
+    
+    public static byte[] fileReader(String path) {
+        byte ret[] = null;
+        File file = new File(path);
+        try {
+            ret = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+    public static byte[] concatArrays(byte[] array1, byte[] array2) {
+        byte[] ret = new byte[array1.length + array2.length];
+        System.arraycopy(array1, 0, ret, 0, array1.length);
+        System.arraycopy(array2, 0, ret, array1.length, array2.length);
+        return ret;
+    }
+    
+}
---
+++ src/guionexamen/KeyManager.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guionexamen;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+/**
+ *
+ * @author 2dam
+ */
+public class KeyManager {
+    
+    //private static final String ALGORITHM = "AES";
+    //private static final String ALGORITHM_STR = "AES/ECB/PKCS5Padding";
+    // cifrado
+    //private static byte[] salt = "esta es la salt!".getBytes();
+    public static String encrypt(String mensaje) throws Exception {
+        PublicKey publicKey;
+        KeyFactory keyFactory;       
+        byte[]key=fileReader("keypublic.txt");
+        byte[] encodedMessage = null;
+        try {
+            keyFactory = KeyFactory.getInstance("RSA");
+            Cipher cipherRSA = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(key);
+            publicKey = keyFactory.generatePublic(spec);
+            cipherRSA.init(Cipher.ENCRYPT_MODE, publicKey);
+            encodedMessage = cipherRSA.doFinal(mensaje.getBytes());
+            
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeySpecException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(KeyFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //We use the method covertStringToHex to get the hex value of the String
+        return hexadecimal(encodedMessage);
+    }
+    // descifrar
+    public static String decrypt(String password) throws Exception {
+        //password=new String(hexStringToByteArray(password));
+        KeyFactory factoriaRSA;
+        //String path=KeyManager.class.getResource("PrivateKey.txt").getPath();
+        PrivateKey privateKey;
+        Cipher cipher;
+        String desc = null;
+        byte[]key = fileReader("keyprivate.txt");
+        try {
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(key);
+            factoriaRSA = KeyFactory.getInstance("RSA");
+            privateKey = factoriaRSA.generatePrivate(spec);
+            cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            //Le decimos que descifre
+            byte[] decodedMessage = cipher.doFinal(hexStringToByteArray(password));
+            // Texto descifrado
+            desc = new String(decodedMessage);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(KeyManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return desc;
+    }
+    
+    private static String convertStringToHex(String password) {
+        StringBuilder hex = new StringBuilder();
+        for(int i=0; i<password.length(); i++){
+            int decimal=(int)i;
+            hex.append(Integer.toHexString(decimal));
+        }
+        return hex.toString();
+    }
+    
+    static String hexadecimal(byte[] resumen) {
+        String hex = "";
+        for (int i = 0; i < resumen.length; i++) {
+            String h = Integer.toHexString(resumen[i] & 0xFF);
+            if (h.length() == 1)
+                    hex += "0";
+            hex += h;
+        }
+        return hex.toUpperCase();
+    }
+    
+    public static byte[] fileReader(String path) {
+        byte ret[] = null;
+        File file = new File(path);
+        try {
+            ret = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+    
+    public static byte[] hexStringToByteArray(String password) {
+        int len = password.length();
+        byte[] mensajeByte = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            mensajeByte[i / 2] = (byte) ((Character.digit(password.charAt(i), 16) << 4)
+                    + Character.digit(password.charAt(i + 1), 16));
+        }
+        return mensajeByte;
+    }
+
+
+    
+}
